5a501fc061a0c3a15b9bf1f1d41fd1041ce8701b
Ahmed Ibrahim <ahmed.mardi09@gmail.com>
2024-11-11 19:27:37 -0500

    Update App Component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,28 @@
-<script>
-  export default {
-    data(){
-      return{
-        name: '-',
-        status: true
-      }
-    }
-  }
+<script lang="ts" setup>
+import { ref } from 'vue';
+import PhotoGallery from './components/PhotoGallery.vue';
+import PhotoFilter from './components/PhotoFilter.vue';
+
+const selectedTags = ref<string[]>([]);
+
+// Handle filter change from PhotoFilter component
+const handleFilterChange = (tags: string[]) => {
+  selectedTags.value = tags;
+};
 </script>
 
 <template>
-  <h1>{{ name }}</h1>
+  <div id="app">
+    <h1>Welcome to Snap Sort</h1>
+
+    <!-- Filtering Component to select tags to filter images -->
+    <PhotoFilter @filter-change="handleFilterChange" />
+
+    <!-- Combined upload and gallery component -->
+    <PhotoGallery :filter-tags="selectedTags" />
+  </div>
 </template>
 
+<style scoped>
+/* Add any necessary global styling here */
+</style>
